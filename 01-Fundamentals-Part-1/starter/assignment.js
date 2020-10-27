8287540b47a31a4baa3347a5bd5bfc9887bf99ac
Tijovanth <58463649+Tijovanth1998@users.noreply.github.com>
2020-10-27 13:59:04 +0530

    update script assignment

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,13 @@
+let country = 'India', continent = 'Asia', population = 130;
+let isIsland = false;
+const language = 'tamil';
+console.log(continent,country,population,isIsland,language);
+console.log(130/2);
+console.log(++population);
+console.log(population > 6);
+console.log(population < 33);
+const decription = country+ " is a country in " + continent + " and it has "+population+" million population";
+console.log(decription);
+
+
+
