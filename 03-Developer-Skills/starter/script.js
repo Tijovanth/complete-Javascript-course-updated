146c7565bf52c93857d031b5cfef437292c70238
Tijovanth <58463649+Tijovanth1998@users.noreply.github.com>
2020-11-07 23:43:01 +0530

    Created Guess My Number Game

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -9,3 +9,16 @@ function multiply(a, b) {
 }
 const result = multiply(1, 2);
 console.log(result);
+
+//Coding Challenge
+
+var printForecast = function (arr) {
+  let result = '...';
+  for (let i = 0; i < arr.length; i++) {
+    result += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  return result;
+};
+
+console.log(printForecast([17, 21, 23]));
+console.log(printForecast([12, 5, -5, 0, 4]));
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,66 @@
 'use strict';
+
+let random;
+let score;
+
+startGuessMyNumberGame();
+function startGuessMyNumberGame() {
+  random = Math.trunc(Math.random() * 20 + 1);
+  console.log(random);
+  document.querySelector('.score').textContent = 20;
+  score = 20;
+  document.querySelector('.message').textContent = 'Start Guessing ...';
+}
+
+document.querySelector('.again').addEventListener('click', function () {
+  startGuessMyNumberGame();
+  document.querySelector('.check').disabled = false;
+  document.querySelector('.number').textContent = '?';
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+  document.querySelector('.guess').value = '';
+  if (!guess) {
+    document.querySelector('.message').textContent = `🚫  No Number`;
+  } else if (guess < 1 || guess > 20) {
+    document.querySelector(
+      '.message'
+    ).textContent = `🚫  Select Number Between 1 to 20`;
+  } else {
+    if (guess === random) {
+      document.querySelector('.message').textContent = `🎉  Correct Number`;
+      document.querySelector('.number').textContent = random;
+      let highscore = document.querySelector('.highscore').textContent;
+      if (highscore < score)
+        document.querySelector('.highscore').textContent = score;
+      document.querySelector('.check').disabled = true;
+    } else if (random > guess) {
+      document.querySelector('.message').textContent = `🤬 Too Low`;
+      decreaseScore();
+    } else if (random < guess) {
+      document.querySelector('.message').textContent = `🤬 Too High`;
+      decreaseScore();
+    }
+  }
+});
+
+function checkScore() {
+  return score > 0 ? true : false;
+}
+
+function decreaseScore() {
+  score -= 1;
+  document.querySelector('.score').textContent = score;
+}
+
+document.querySelector('.score').addEventListener(
+  'DOMSubtreeModified',
+  function () {
+    if (!checkScore()) {
+      document.querySelector('.check').disabled = true;
+      document.querySelector('.message').textContent = `😭 You Lost`;
+    }
+  },
+  false
+);
