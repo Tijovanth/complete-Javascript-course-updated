ac61762b77f28c2c9a65f291d6153eca7f0c5a84
Tijovanth <58463649+Tijovanth1998@users.noreply.github.com>
2020-11-10 02:11:17 +0530

    update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+function calAge(birthYear) {
+  const age = 2020 - birthYear;
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1986 && birthYear <= 2000) {
+      var millenial = true;
+      output = 'NEW OUTPUT!';
+      const firstName = 'Steven';
+      function add(a, b) {
+        return a + b;
+      }
+      const str = `Oh, you are millienal, ${firstName}`;
+      console.log(str);
+    }
+    //console.log(add(2, 3));
+    console.log(output);
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+const firstName = `tijo`;
+calAge(1998);
