05eb35440c26b543f0cf3e1ee672cc4ed9a96e65
Tijovanth <58463649+Tijovanth1998@users.noreply.github.com>
2020-11-18 21:24:40 +0530

    did behind the scenes and data structure operation started

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,26 +1,194 @@
 'use strict';
-function calAge(birthYear) {
-  const age = 2020 - birthYear;
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+// function calAge(birthYear) {
+//   const age = 2020 - birthYear;
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1986 && birthYear <= 2000) {
-      var millenial = true;
-      output = 'NEW OUTPUT!';
-      const firstName = 'Steven';
-      function add(a, b) {
-        return a + b;
+//     if (birthYear >= 1986 && birthYear <= 2000) {
+//       var millenial = true;
+//       output = 'NEW OUTPUT!';
+//       const firstName = 'Steven';
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       const str = `Oh, you are millienal, ${firstName}`;
+//       console.log(str);
+//     }
+//     //console.log(add(2, 3));
+//     console.log(output);
+//     console.log(millenial);
+//   }
+//   printAge();
+//   return age;
+// }
+// const firstName = `tijo`;
+// calAge(1998);
+
+///////////////////////////////////////
+// Hoisting and TDZ in Practice
+
+// console.log(firstname);
+//console.log(job);
+//console.log(age);
+
+// display();
+// //display2();
+// //display3();
+
+// var firstname = 'tijo';
+// let job = 'developer';
+// const age = 21;
+
+// function display() {
+//   console.log('This is a display Function declaration');
+// }
+
+// let display2 = function () {
+//   console.log('This is a function expression of display 2 function');
+// };
+
+// const display3 = () => console.log(1 + 2);
+
+// // Real time Example of why in most cases we should not use var
+// console.log(undefined);
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted!');
+// }
+
+// In non-strict mode we can use variable without declaring. It will add to window object. But is not true in strict mode it will throw an error
+//x = 12;
+//z = 13;
+
+//This keyword for normal Function
+//1.For  Function declaration and function expression this keyword refers to window object in sloppy mode but in strict mode this refers to undefined
+//2.For global scope this keyword refers to window object both in sloppy mode and strict mode
+//3.For methods this refers to who calls that method
+// console.log(this);
+// function calAge(birthYear) {
+//   console.log(this);
+//   return 2020 - birthYear;
+// }
+
+// calAge(1998);
+
+// var tijo = {
+//   firstName: 'tijo',
+//   lastName: 'vanth',
+//   display: function () {
+//     console.log(`${this.firstName}${this.lastName}`);
+//   },
+// };
+
+// tijo.display();
+
+// var someone = {
+//   firstName: 'ali',
+//   lastName: 'khan',
+// };
+// someone.display = tijo.display;
+// someone.display();
+
+// //this keyword in arrow function
+// //1. For arrow function this refers to its parents scope that is called lexical scope
+// // In this example this refers to parent scope that is global object
+// var mom = {
+//   firstName: 'Shantha',
+//   lastName: 'latha',
+//   displayMomName: () => console.log(`${this.firstName}${this.lastName}`),
+// };
+// mom.displayMomName();
+
+// //when should not use arrow function
+// // if we use var for creating variable in global scope it will add that variable as a property in windows object
+// // if at that time if arrow function parent scope is globel then it will create some problem
+// var firstName = 'Someone';
+// var dad = {
+//   firstName: 'Ranga',
+//   displayDadName: () => console.log(`${this.firstName}`),
+//   Solution: function () {
+//     console.log(`${this.firstName}`);
+//   },
+// };
+// dad.displayDadName();
+// dad.Solution();
+
+// When can we use arrow function
+// in function inside method. That function this key refers to undefined when you are in strict mode.
+// so it will create some problem
+// so we can use arrow function for that as i said earlier arrow don't have specific this keyword so it refers its parent scope
+var tijo = {
+  firstName: 'tijo',
+  year: 1998,
+  calAge: function () {
+    console.log(2020 - this.year);
+    function isMillenial() {
+      if (this.year >= 1986 && this.year <= 1999) {
+        console.log(`${this.firstName} ya you are millenial`);
+      } else {
+        console.log(`you are not`);
       }
-      const str = `Oh, you are millienal, ${firstName}`;
-      console.log(str);
     }
-    //console.log(add(2, 3));
-    console.log(output);
-    console.log(millenial);
-  }
-  printAge();
-  return age;
-}
-const firstName = `tijo`;
-calAge(1998);
+    // const isMillenial = () => {
+    //   if (this.year >= 1986 && this.year <= 1999) {
+    //     console.log(`${this.firstName} ya you are millenial`);
+    //   } else {
+    //     console.log(`you are not`);
+    //   }
+    // };
+    isMillenial();
+  },
+};
+
+// tijo.calAge();
+
+// arguments keyword
+// for arrow function arguments keyword is not available
+const add = function (a, b) {
+  console.log(arguments);
+  console.log(arguments[0] + arguments[2]);
+};
+//add(1, 2, 3, 4);
+
+const sub = (a, b) => {
+  console.log(arguments);
+};
+//sub();
+///////////////////////////////////////
+// Primitives vs. Objects in Practice
+//Primitives
+let age = 30;
+let oldAge = age;
+age = 31;
+// console.log(age);
+// console.log(oldAge);
+//Reference
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+// console.log('Before marriage:', jessica);
+// console.log('After marriage: ', marriedJessica);
+//marriedJessica = {};
+
+// Copying objects
+// Object.assign will do shallow copy so first level of properties will be preserved
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+console.log('Before marriage:', jessica2);
+console.log('After marriage: ', jessicaCopy);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -6,6 +6,33 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starerIndex, mainIndex, mainIndex1) {
+    return [
+      this.starterMenu[starerIndex],
+      this.mainMenu[mainIndex],
+      this.mainMenu[mainIndex1],
+    ];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 1,
+    time = '20:30',
+    address,
+  }) {
+    console.log(
+      this.starterMenu[starterIndex],
+      this.mainMenu[mainIndex],
+      time,
+      address
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 
   openingHours: {
     thu: {
@@ -22,3 +49,95 @@ const restaurant = {
     },
   },
 };
+
+///////////////////////////////////////
+// Destructuring Arrays
+// let arr = [1, 2];
+// let [x, y] = arr;
+// console.log(x, y);
+
+// //Skipping values
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// //Swaping Values
+// [secondary, main] = [main, secondary];
+// console.log(main, secondary);
+
+// //Receive more than one value from function
+// let [starter, Main, Main1] = restaurant.order(2, 1, 2);
+// console.log(starter, Main, Main1);
+
+// //Nester destructing
+// let nested = [1, 2, 3, [4, 5]];
+// let [one, , three, [four, five]] = nested;
+// console.log(one, three, four, five);
+
+// //Default Values
+// let arr1 = [1, 2];
+// let [p, , r = 1] = arr1;
+// console.log(p, r);
+
+///////////////////////////////////////
+// Destructuring Objects
+
+// Default Values
+// let {
+//   name,
+//   location: a = 1,
+//   categories: b = 2,
+//   openingHours: c = 3,
+// } = restaurant;
+// console.log(name, a, b, c);
+
+// // Mutating Values
+// let x = 12;
+// let y = 13;
+// const obj = { x: 23, y: 7, z: 14 };
+// ({ x = 1, y = 2 } = obj);
+// console.log(x, y);
+
+// //Nested Objects
+// const {
+//   fri: { open: op = 1, close: cl = 2 },
+// } = c;
+// console.log(op, cl);
+
+// // Passing object has an function parameter and their receiving destructing is happening
+// const order = {
+//   address: '172/83',
+// };
+// restaurant.orderDelivery(order);
+
+///////////////////////////////////////
+// The Spread Operator (...)
+
+const arr = [1, 2, 3];
+const newarr = [...arr, 4, 5];
+console.log(...newarr);
+
+// Copy array
+const arr1 = [12, 2, 3, 4, 5];
+const arr2 = [...arr1];
+arr1[0] = 1;
+console.log(arr1, arr2);
+
+//join 2 arrays
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+// Iterables: arrays, strings, maps, sets. NOT objects but after es2018 we can use spread operator in objects also
+const str = 'tijovanth';
+console.log(...str);
+
+// using spread operator for function arguments
+const ingrediants = ['pepper', 'macroni', 'salt'];
+restaurant.orderPasta(...ingrediants);
+
+//using objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
